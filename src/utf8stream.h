e755f544ef5e31c86a4bc3f449c3f1414f1232f9
Andreas Fett <a.fett@gmx.de>
2018-02-26 01:06:34 +0100

    Utf8Stream: inline class

--- src/utf8stream.h
+++ src/utf8stream.h
@@ -1,8 +1,12 @@
 /*
-   Copyright (c) 2015, 2016, Andreas Fett. All rights reserved.
+   Copyright (c) 2015, 2016, 2018 Andreas Fett. All rights reserved.
    Use of this source code is governed by a BSD-style
    license that can be found in the LICENSE file.
 */
+
+#ifndef JSON_UTF8_STREAM_H
+#define JSON_UTF8_STREAM_H
+
 #include <jsoncc.h>
 #include "utf8.h"
 
@@ -16,12 +20,74 @@ public:
 		SBAD = -2,
 	};
 
-	Utf8Stream(const char *, size_t);
-	State state() const;
-	int getc(); // throws jsonp::Error
-	void ungetc();
-	Location location() const;
-	void bad();
+	Utf8Stream(const char *buf, size_t len)
+	:
+		buf_(buf),
+		len_(len),
+		pos_(0),
+		bad_(false),
+		eof_(false),
+		utf8_(),
+		valid_(0)
+	{ }
+
+	State state() const
+	{
+		if (bad_) {
+			return SBAD;
+		}
+
+		if (eof_) {
+			return SEOF;
+		}
+
+		return SGOOD;
+	}
+
+	int getc()
+	{
+		if (bad_) {
+			return SBAD;
+		}
+
+		if (pos_ == len_) {
+			eof_ = true;
+			return SEOF;
+		}
+
+		uint8_t c(buf_[pos_]);
+		if (c == '\0') {
+			bad_ = true;
+			throw Error(Error::STREAM_ZERO, location());
+		}
+
+		if (valid_ <= pos_) {
+			if ((bad_ = !utf8_.validate(c))) {
+				throw Error(Error::UTF8_INVALID, location());
+			}
+			++valid_;
+		}
+
+		++pos_;
+		return c;
+	}
+
+	void ungetc()
+	{
+		if (pos_ != 0 && !bad_ && !eof_) {
+			--pos_;
+		}
+	}
+
+	Location location() const
+	{
+		return Location(pos_);
+	}
+
+	void bad()
+	{
+		bad_ = true;
+	}
 
 private:
 	const char *buf_;
@@ -34,3 +100,4 @@ private:
 };
 
 }
+#endif
